1df290f9457886ea2b0b4d536f000c3aba562191
adri1samp <adri1pawn@gmail.com>
2020-04-16 03:44:47 +0200

    BoxTextInput setError message

--- src/components/BoxTextInput.js
+++ src/components/BoxTextInput.js
@@ -1,6 +1,6 @@
 //Imports
 import React from "react";
-import { TextInput } from "react-native";
+import { View, TextInput, Text } from "react-native";
 
 //Styles Imports
 import Styles from "cuervo/src/utils/Styles";
@@ -14,7 +14,8 @@ export default class BoxButton extends React.Component {
         super(props);
         this.state = {
             focused: false,
-            text: ""
+            text: "",
+            errorText: null
         };
     }
     
@@ -26,26 +27,48 @@ export default class BoxButton extends React.Component {
         this.setState({ text: value })
     }
 
+    setError(value) {
+        this.setState({ errorText: value });
+    }
+
+    renderErrorMessage() {
+        if(this.state.errorText != null) {
+            return (
+                <Text style={[
+                    Styles.smallText,
+                    { color: "red" }
+                ]}>{ this.state.errorText }</Text>
+            );
+        }
+    }
+
     render () {
         return (
-            <TextInput
-                style={[
-                    {
-                        borderRadius: 1,
-                        borderWidth: 1,
-                        padding: Definitions.DEFAULT_MARGIN / 2,
-                        paddingLeft: Definitions.DEFAULT_MARGIN,
-                        paddingRight: Definitions.DEFAULT_MARGIN,
-                        marginBottom: Definitions.DEFAULT_MARGIN
-                    },
-                    Styles.normalText,
-                    this.state.focused ? { borderColor: "rgba(255, 255, 255, 1.0);" } : { borderColor: "rgba(120, 120, 120, 0.2);" }
-                ]}
-                placeholderTextColor={ "rgba(255, 255, 255, 0.4);" }
-                placeholder={ this.props.placeholder }
-                secureTextEntry={ this.props.secureTextEntry }
-                value= { this.state.text }
-            />
+            <View style={{
+                marginBottom: Definitions.DEFAULT_MARGIN
+            }}>
+                <TextInput
+                    style={[
+                        {
+                            borderRadius: 1,
+                            borderWidth: 1,
+                            padding: Definitions.DEFAULT_MARGIN / 2,
+                            paddingLeft: Definitions.DEFAULT_MARGIN,
+                            paddingRight: Definitions.DEFAULT_MARGIN
+                        },
+                        Styles.normalText,
+                        this.state.focused ? { borderColor: "rgba(255, 255, 255, 1.0);" } : { borderColor: "rgba(120, 120, 120, 0.2);" }
+                    ]}
+                    placeholderTextColor={ "rgba(255, 255, 255, 0.4);" }
+                    placeholder={ this.props.placeholder }
+                    secureTextEntry={ this.props.secureTextEntry }
+                    value={ this.state.text }
+                    maxLength={ this.props.maxLength }
+                />
+                {
+                    this.renderErrorMessage()
+                }
+            </View>
         );
     }
 }
